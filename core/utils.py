7e7dc761f9116e25b8daa65d1d1de1c3fbb5d595
JulioGalindoC <69281523+JulioGalindoC@users.noreply.github.com>
2021-02-22 09:24:47 -0300

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -214,6 +214,7 @@ def format_boxes(bboxes, image_height, image_width):
     return bboxes
 
 def draw_bbox(image, bboxes, info = False, counted_classes = None, show_label=True, allowed_classes=list(read_class_names(cfg.YOLO.CLASSES).values()), read_plate = False):
+    o = open('BBox_Coords.txt', 'w')
     classes = read_class_names(cfg.YOLO.CLASSES)
     num_classes = len(classes)
     image_h, image_w, _ = image.shape
@@ -249,7 +250,7 @@ def draw_bbox(image, bboxes, info = False, counted_classes = None, show_label=Tr
             cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
 
             if info:
-                print("{}, Confidence: {:.2f}, BBox Inf. Coords (xmin, ymin, xmax, ymin): {}, {}, {}, {} ".format(class_name, score, coor[0], coor[1], coor[2], coor[1]))
+                o.write("{}, Confidence: {:.2f}, BBox Inf. Coords (xmin, ymin, xmax, ymin): {}, {}, {}, {} ".format(class_name, score, coor[0], coor[1], coor[2], coor[1]))
 
             if show_label:
                 bbox_mess = '%s: %.2f' % (class_name, score)
